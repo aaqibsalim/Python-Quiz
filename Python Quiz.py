efe642c115e3482ab79cd2c4c80c54c1f9426c76
aaqibsalim <aaqib.salim@education.nsw.gov.au>
2025-02-21 18:39:23 +1100

    Add files via upload

---
+++ Python Quiz.py
@@ -0,0 +1,11 @@
+import tkinter as tk
+from tkinter import *
+from tkinter.ttk import *
+
+root = tk.Tk()
+root.title('Python Quiz')
+root.geometry("1920x1080")
+icon = tk.PhotoImage('Python Icon.png')
+root.iconphoto(False, icon)
+
+root.mainloop()
